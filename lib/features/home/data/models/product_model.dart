8496dd1b9b0dd0c36958695ed03dc1905aed26fd
islam ahmed <islamahmedkamel592@gmail.com>
2026-07-17 03:43:16 +0300

    create product model

---
+++ lib/features/home/data/models/product_model.dart
@@ -0,0 +1,121 @@
+import 'package:e_commerece_app/features/home/data/models/catygroy_model.dart';
+
+class ProductModel {
+  int? sold;
+  List<String>? images;
+  List<Subcategory>? subcategory;
+  int? ratingsQuantity;
+  String? sId;
+  String? title;
+  String? slug;
+  String? description;
+  int? quantity;
+  int? price;
+  String? imageCover;
+  Category? category;
+  Category? brand;
+  double? ratingsAverage;
+  String? createdAt;
+  String? updatedAt;
+  String? id;
+
+  ProductModel(
+      {this.sold,
+      this.images,
+      this.subcategory,
+      this.ratingsQuantity,
+      this.sId,
+      this.title,
+      this.slug,
+      this.description,
+      this.quantity,
+      this.price,
+      this.imageCover,
+      this.category,
+      this.brand,
+      this.ratingsAverage,
+      this.createdAt,
+      this.updatedAt,
+      this.id});
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    sold = json['sold'];
+    images = json['images'].cast<String>();
+    if (json['subcategory'] != null) {
+      subcategory = <Subcategory>[];
+      json['subcategory'].forEach((v) {
+        subcategory!.add(new Subcategory.fromJson(v));
+      });
+    }
+    ratingsQuantity = json['ratingsQuantity'];
+    sId = json['_id'];
+    title = json['title'];
+    slug = json['slug'];
+    description = json['description'];
+    quantity = json['quantity'];
+    price = json['price'];
+    imageCover = json['imageCover'];
+    category = json['category'] != null
+        ? new Category.fromJson(json['category'])
+        : null;
+    brand = json['brand'] != null ? new Category.fromJson(json['brand']) : null;
+    ratingsAverage = json['ratingsAverage'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['sold'] = this.sold;
+    data['images'] = this.images;
+    if (this.subcategory != null) {
+      data['subcategory'] = this.subcategory!.map((v) => v.toJson()).toList();
+    }
+    data['ratingsQuantity'] = this.ratingsQuantity;
+    data['_id'] = this.sId;
+    data['title'] = this.title;
+    data['slug'] = this.slug;
+    data['description'] = this.description;
+    data['quantity'] = this.quantity;
+    data['price'] = this.price;
+    data['imageCover'] = this.imageCover;
+    if (this.category != null) {
+      data['category'] = this.category!.toJson();
+    }
+    if (this.brand != null) {
+      data['brand'] = this.brand!.toJson();
+    }
+    data['ratingsAverage'] = this.ratingsAverage;
+    data['createdAt'] = this.createdAt;
+    data['updatedAt'] = this.updatedAt;
+    data['id'] = this.id;
+    return data;
+  }
+}
+
+class Subcategory {
+  String? sId;
+  String? name;
+  String? slug;
+  String? category;
+
+  Subcategory({this.sId, this.name, this.slug, this.category});
+
+  Subcategory.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    name = json['name'];
+    slug = json['slug'];
+    category = json['category'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['name'] = this.name;
+    data['slug'] = this.slug;
+    data['category'] = this.category;
+    return data;
+  }
+}
+
